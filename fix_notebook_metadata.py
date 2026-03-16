92ef094f2fa0d61a521d9a3b0bdb8a52e1598f61
sucharitha1812 <sucharitha1812@gmail.com>
2026-03-15 23:27:09 -0400

    Organize project structure

---
+++ fix_notebook_metadata.py
@@ -0,0 +1,15 @@
+import json
+
+notebook_path = "ML Project.ipynb"
+
+with open(notebook_path, "r", encoding="utf-8") as f:
+    nb = json.load(f)
+
+# Remove problematic widget metadata but keep outputs
+if "metadata" in nb and "widgets" in nb["metadata"]:
+    del nb["metadata"]["widgets"]
+
+with open(notebook_path, "w", encoding="utf-8") as f:
+    json.dump(nb, f, indent=1, ensure_ascii=False)
+
+print("Removed metadata.widgets and kept outputs.")
